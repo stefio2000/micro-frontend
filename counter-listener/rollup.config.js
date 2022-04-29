d7e56b2a78c27951518d87d6ecc39e74269a6c48
DAVID Stéphane <stef.david.dev@gmail.com>
2022-04-29 09:42:49 +0200

    web component in react + full build

---
+++ counter-listener/rollup.config.js
@@ -0,0 +1,46 @@
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+
+const OUTPUT_FOLDER = 'build/';
+const DOC_FOLDER = 'docs/';
+
+const production = process.env.NODE_ENV === 'prod';
+
+const plugins = [
+  resolve({
+    browser: true,
+    extensions: ['.js', '.jsx', '.json'],
+  }),
+  babel({
+    presets: ['@babel/preset-react'],
+    exclude: 'node_modules/**',
+  }),
+  commonjs(),
+  replace({
+    'process.env.NODE_ENV': production
+      ? JSON.stringify('production')
+      : JSON.stringify('development'),
+  }),
+];
+
+export default [{
+  input: 'src/index.jsx',
+  output: {
+    file: `${OUTPUT_FOLDER}counter-listener.js`,
+    format: 'iife',
+    sourcemap: true,
+  },
+  plugins,
+},
+{
+  input: 'src/index.jsx',
+  output: {
+    file: `${DOC_FOLDER}counter-listener.js`,
+    format: 'iife',
+    sourcemap: true,
+  },
+  plugins,
+},
+];
